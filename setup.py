f1a44a6e2dc0189f1b9ea84ac411767497347dee
Tran Minh Tien <tmt31032005@gmail.com>
2025-08-10 22:09:57 +0700

    initialize folder

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot Project',
+    version= '0.0.0',
+    author= 'Tien Tran Minh',
+    author_email= 'tmt31032005@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
